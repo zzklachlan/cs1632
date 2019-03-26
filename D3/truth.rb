b1afbed2351c4041402db1d67493a59e6d423872
zzklachlan <zzklanchlan@gmail.com>
2019-03-26 18:16:11 -0400

    first try

---
+++ D3/truth.rb
@@ -0,0 +1,35 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+is_valid = true
+
+def check_params(ts, fs, size)
+  if !ts.nil? && ts.length != 1
+    is_valid = false
+  elsif !fs.nil? && fs.length != 1
+    is_valid = false
+  elsif !ts.nil? && !fs.nil? && ts == fs
+    is_valid = false
+  # elsif !size.nil? && size.to_i < 2
+  #   is_valid = false
+  end
+  is_valid
+end
+
+get '/' do
+  ts = params['ts']
+  fs = params['fs']
+  size = params['size']
+
+  # if !size.nil?
+  #   puts "----------#{size.to_i}-------------"
+  # end
+
+  # puts "-----------#{is_valid}-----------------"
+  if check_params ts, fs, size == false
+    status 404
+    erb :error
+  else
+    erb :index, :locals => { ts: ts, fs: fs, size: size}
+  end
+end
