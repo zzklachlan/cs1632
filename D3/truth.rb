185aec24c39e083f170fea33064a751d375df5ad
laboon <laboon@gmail.com>
2019-03-27 09:37:34 -0400

    please review

--- D3/truth.rb
+++ D3/truth.rb
@@ -4,23 +4,27 @@ require 'sinatra/reloader'
 is_valid = true
 
 def check_params(ts, fs, size)
-  if !ts.nil? && ts.length != 1
+  puts 'check_params called..'
+  return false if ts.nil? || fs.nil? || size.nil?
+  if ts.length != 1
     is_valid = false
-  elsif !fs.nil? && fs.length != 1
+  elsif fs.length != 1
+    puts 'cp: 2'
     is_valid = false
-  elsif !ts.nil? && !fs.nil? && ts == fs
+  elsif ts == fs
+    puts 'cp: 3'
     is_valid = false
   # elsif !size.nil? && size.to_i < 2
-  #   is_valid = false
+    #   is_valid = false
+  else
+    puts 'cp: 4'
+    is_valid = true
   end
+  puts "returning #{is_valid}"
   is_valid
 end
 
 post '/display' do
-  erb :display
-end
-
-get '/' do
   ts = params['ts']
   fs = params['fs']
   size = params['size']
@@ -30,10 +34,16 @@ get '/' do
   # end
 
   # puts "-----------#{is_valid}-----------------"
-  if check_params ts, fs, size == false
+  puts params
+  if check_params(ts, fs, size)
+    erb :display, :locals => { ts: ts, fs: fs, size: size}
+  else
     status 404
     erb :error
-  else
-    erb :index, :locals => { ts: ts, fs: fs, size: size}
   end
+
+end
+
+get '/' do
+  erb :index
 end
